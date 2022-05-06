15be7d675260bbae7f9e834d41e41cbfc6ab12ea
femmyte <sanyaoluadefemi@gmail.com>
2022-05-06 10:13:55 +0100

    created GameOverPage

---
+++ pages/GameOverPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View, StyleSheetk } from 'react-native'
+import Card from '../components/Card'
+const GameOverPage = () => {
+  return (
+    <View>
+      <Card>
+        <Text>Game Over</Text>
+      </Card>
+    </View>
+  )
+}
+
+export default GameOverPage
