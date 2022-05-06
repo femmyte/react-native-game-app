45829a5fe7bd4d6415e5e32d2965072138924a25
femmyte <sanyaoluadefemi@gmail.com>
2022-05-06 10:23:30 +0100

    styled the game over page

--- pages/GameOverPage.js
+++ pages/GameOverPage.js
@@ -1,14 +1,33 @@
 import React from 'react'
-import { Text, View, StyleSheetk } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
+import color from '../color'
 import Card from '../components/Card'
 const GameOverPage = () => {
   return (
-    <View>
-      <Card>
-        <Text>Game Over</Text>
+    <View style={styles.screen}>
+      <Card style={styles.card}>
+        <Text style={styles.text}>Game Over</Text>
       </Card>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  screen: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '80%',
+  },
+  card: {
+    width: '50%',
+    height: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: color.primary,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+})
 
 export default GameOverPage
